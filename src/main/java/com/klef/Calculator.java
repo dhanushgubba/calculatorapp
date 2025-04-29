331546d19f1114f55f72b02db329d4e90510e878
dhanushgubba <dhanushgubba.ap@gmail.com>
2025-04-29 11:18:09 +0530

    commit

---
+++ src/main/java/com/klef/Calculator.java
@@ -0,0 +1,50 @@
+package com.klef;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calculator
+ */
+public class Calculator extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Calculator() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int A = Integer.parseInt(request.getParameter("V1"));
+		int B = Integer.parseInt(request.getParameter("V2"));
+		int C = A+B;
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.print("SUM =" + C);
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int A = Integer.parseInt(request.getParameter("V1"));
+		int B = Integer.parseInt(request.getParameter("V2"));
+		int C = A-B;
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.print("SUB =" + C);
+
+	}
+
+}
